e6831cc0caab6684bcbb641541f3ea30d1f096ed
yuimarudev <yuimarudev@outlook.jp>
2023-08-01 22:54:13 +0900

    fix og

--- src/routes/embed/+page.svelte
+++ src/routes/embed/+page.svelte
@@ -10,5 +10,6 @@
   <meta property="og:url" content={data.url} />
   <meta property="og:image" content={data.imageUrl} />
   <meta property="og:site_name" content={data.siteName} />
+  <meta property="og:description" content={data.description} />
   <meta name="theme-color" content={data.color} />
 </svelte:head>
